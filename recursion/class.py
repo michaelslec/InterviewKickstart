52c5fca1b873f43a8beca504fd0d896a103857d2
Michael LeCompte <michaelslec98@gmail.com>
2020-12-17 14:19:24 -0800

    feat: use mutable slate variant

--- recursion/class.py
+++ recursion/class.py
@@ -8,7 +8,6 @@ def capital_combos(problem: str):
 
     def helper(start: int, slate: str):
         if start == n:
-            print(slate)
             results.append(slate)
             # time.sleep(0.003)
         else:
@@ -23,4 +22,40 @@ def capital_combos(problem: str):
     return results
 
 
-capital_combos("a1bc")
+def capital_combos_mutable(problem: str):
+    results = []
+    slate = []
+    n = len(problem)
+
+    def helper(start: int):
+        if start == n:
+            results.append("".join(slate))
+            # time.sleep(0.003)
+        else:
+            if not problem[start].isalpha():
+                slate.append(problem[start])
+                helper(start + 1)
+                slate.pop(-1)
+            else:
+                slate.append(problem[start])
+                helper(start + 1)
+                slate.pop(-1)
+
+                slate.append(problem[start].upper())
+                helper(start + 1)
+                slate.pop(-1)
+
+    helper(0)
+
+    return results
+
+
+start = time.time()
+capital_combos("abcdefghiklmnopqrstuv")
+end = time.time()
+print(f"immutable: {end - start}")
+
+start = time.time()
+capital_combos_mutable("abcdefghiklmnopqrstuv")
+end = time.time()
+print(f"mutable: {end - start}")
