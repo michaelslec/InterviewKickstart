545220abd128050fe54c23d590cf24205462ca0c
Michael LeCompte <michaelslec98@gmail.com>
2020-12-17 13:17:16 -0800

    feat: applying general recursion template

---
+++ recursion/class.py
@@ -0,0 +1,26 @@
+#!/bin/python
+import time
+
+
+def capital_combos(problem: str):
+    results: list = []
+    n: int = len(problem)
+
+    def helper(start: int, slate: str):
+        if start >= n:
+            print(slate)
+            results.append(slate)
+            time.sleep(0.003)
+        else:
+            if problem[start].isnumeric():
+                helper(start + 1, slate + problem[start])
+            else:
+                helper(start + 1, slate + problem[start])
+                helper(start + 1, slate + problem[start].upper())
+
+    helper(0, "")
+
+    return results
+
+
+capital_combos("abcdefghjiklm")
